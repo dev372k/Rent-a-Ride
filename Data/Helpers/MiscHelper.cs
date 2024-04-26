5583ca40177d0b67580428d81e7f80faf10f1ab7
S.M.Owais <smowais.neduet@gmail.com>
2024-04-26 18:53:59 +0500

    minor changes

---
+++ Data/Helpers/MiscHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Helpers
+{
+    public class MiscHelper
+    {
+        public static string FormatNumber(double number)
+        {
+            const double Million = 1000000;
+            const double Thousand = 1000;
+
+            if (number >= Million)
+                return (number / Million).ToString("0.##M");
+            else if (number >= Thousand)
+                return (number / Thousand).ToString("0.##K");
+            else
+                return number.ToString();
+        }
+    }
+}
